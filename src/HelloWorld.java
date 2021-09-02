28ec674f44038716c9f5b86ac06e7358ee53616a
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-02 16:42:20 -0500

    feat: java lecture

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+
+    public static void main
+            (String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is a great day to code!");
+        System.out.print("Three...");
+        System.out.print("Two...");
+    }
+
+
+}
