d006fcc6419394bae6fea1887912529b5e824d58
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-03 10:27:38 -0500

    feat: java exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "Hello";
+//        System.out.println(myString);
+
+//        long myNumber;
+//        myNumber = 123L;
+
+//        float myNumber = 3.14F;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "Hello";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+        // #13
+//        int x =4;
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x/=y;
+        y -= x;
+
+        System.out.println(Integer.MAX_VALUE);
+
+   }
+
+
+
+
+
+
+
+
+
 }
