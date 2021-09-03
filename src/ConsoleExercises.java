7295b2ef2b5ff3430a0689c937019027c01b06fd
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-03 15:11:37 -0500

    feat: added bonus problems

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -33,7 +33,25 @@ public class ConsoleExercises {
         System.out.println("Area of rectangle is: " + area );
 
 
-
+//# Console IO Bonus Exercises
+//
+//        1. Prompt the user to enter a favorite quote
+//        - output the quote
+//        - ask them to enter how many words are in the quote
+//        - output the number they entered
+//        - output if the number they entered indeed matched the number of words
+//
+//        2. Prompt the user to enter a list of top three favorite foods separated by only spaces
+//
+//        - Use the printf() to output the three top foods with the format:
+//        1. FirstFood
+//        2. SecondFood
+//        3. ThirdFood
+//
+//        3. Prompt the user to enter a grocery list of three items
+//                - each item should only be separated by a comma (no spaces)
+//        - you will need to use the .useDelimiter() method on your scanner object
+//                - output the result as a comma-separated list using printf()
 
     }
 }
