eb7c6810ad2d5785a19798be9c79759834c80c56
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-08 09:30:24 -0500

    feat: fixed lower case issue

--- src/MyTherapist.java
+++ src/MyTherapist.java
@@ -1,16 +1,17 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class MyTherapist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How are you doing? ");
-        String myAnswer = scanner.nextLine();
+        String myAnswer = scanner.nextLine().toLowerCase();
         switch (myAnswer) {
-            case "great" -> System.out.println("Amazing to see the positivity!");
-            case "good" -> System.out.println("The day will only get better, stay positive!");
-            case "okay" -> System.out.println("Stay the course!");
-            case "not bad" -> System.out.println("Perhaps the day will get better.");
-            case "not good" -> System.out.println("What can we do to make it better?");
-            case "bad" -> System.out.println("Come see me today.");
+            case "Great" -> System.out.println("Amazing to see the positivity!");
+            case "Good" -> System.out.println("The day will only get better, stay positive!");
+            case "Okay" -> System.out.println("Stay the course!");
+            case "Not bad" -> System.out.println("Perhaps the day will get better.");
+            case "Not good" -> System.out.println("What can we do to make it better?");
+            case "Bad" -> System.out.println("Come see me today.");
             default -> System.out.println("We don't have that");
         }
     }
