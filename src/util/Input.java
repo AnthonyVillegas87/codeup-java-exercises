62f017e5bc227e31bae2ef84955418ac6a6ba5a0
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-10 15:52:45 -0500

    feat: Input.java

--- src/util/Input.java
+++ src/util/Input.java
@@ -13,6 +13,38 @@ public class Input {
        this.scanner = new Scanner(System.in);
    }
 
+   boolean yesNo(){
+       System.out.println("Enter yes or y: ");
+       String userInput = scanner.nextLine().toLowerCase();
+       return (userInput.equals("yes") || userInput.equals("y"));
+   }
+
+   int getInt(int min, int max) {
+       System.out.println("Enter a number between " + min + " and " + max);
+       int input = scanner.nextInt();
+       if(input < min || input > max) {
+           getInt();
+       }
+       return input;
+   }
+
+   int getInt() {
+     return getInt(1, 10);
+   }
+
+   double getDouble(double min, double max) {
+       System.out.println("Enter a number between " + min + " and " + max);
+       double dblInput = scanner.nextInt();
+       if(dblInput < min || dblInput > max) {
+           getDouble();
+       }
+       return dblInput;
+   }
+
+   double getDouble() {
+       return getDouble(1, 10);
+   }
+
 
 
 
