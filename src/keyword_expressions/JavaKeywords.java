3db4ab51cb218480786d99c50296c61a4e09dbd5
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-13 15:55:27 -0600

    feat: methods challenge pt 1

--- src/keyword_expressions/JavaKeywords.java
+++ src/keyword_expressions/JavaKeywords.java
@@ -2,29 +2,58 @@ package keyword_expressions;
 
 public class JavaKeywords {
 
+    ///Methods
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
+        }
+        return -1;
+
+
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if(score >= 1000) {
+           return 1;
+        } else if (score >= 500 && score < 1000){
+            return 2;
+        } else if (score >= 100 &&  score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+
+    }
 
     public static void main(String[] args) {
         //===== Expressions
         // a mile is equal to 1.609344 kilometers
         double kilometers = (100 * 1.609344);
 
-       boolean gameOver = true;
-       int score = 5000;
-       int levelCompleted = 5;
-       int bonus = 100;
+        calculateScore(true, 800, 5, 100);
 
-     if(gameOver) {
-         int finalScore = score + (levelCompleted * bonus);
-         System.out.println("Your final score : " + finalScore);
-     }
+        calculateScore(true, 10000, 8, 200);
 
-     score = 10000;
-     levelCompleted = 8;
-     bonus = 200;
-     if (gameOver) {
-        int newScore = score + (levelCompleted * bonus);
-         System.out.println("Your new score : " + newScore);
-     }
+
+       int highScoreResult = calculateHighScorePosition(1500);
+       displayHighScorePosition("John", highScoreResult);
+
+       highScoreResult = calculateHighScorePosition(900);
+        displayHighScorePosition("Lucy", highScoreResult);
+
+       highScoreResult = calculateHighScorePosition(400);
+       displayHighScorePosition("Joey", highScoreResult);
+
+       highScoreResult = calculateHighScorePosition(50);
+       displayHighScorePosition("George", highScoreResult);
 
     }
+
+
 }
