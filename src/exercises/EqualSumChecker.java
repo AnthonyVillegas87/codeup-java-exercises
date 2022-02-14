f43ed4724d63b96cfc8e93d5aacf2ec1a1536f8e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-14 11:06:48 -0600

    feat: equal sum checker exercise

---
+++ src/exercises/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int x, int y, int z) {
+
+        return x + y == z;
+
+    }
+}
---
+++ src/exercises/EqualSumTest.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class EqualSumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 1));
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 2));
+        System.out.println(EqualSumChecker.hasEqualSum(1, -1, 0));
+    }
+}
