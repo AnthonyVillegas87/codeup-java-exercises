edc161097730bdd7d4e9f0493d05c2caa16a79d5
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-14 14:05:49 -0600

    feat: seconds and minutes exercise

---
+++ src/exercises/SecondsAndMinutesTest.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class SecondsAndMinutesTest {
+
+    public static void main(String[] args) {
+        System.out.println(SecondsAndMinutes.getDurationString(65, 45));
+
+        System.out.println(SecondsAndMinutes.getDurationString(3945));
+    }
+}
