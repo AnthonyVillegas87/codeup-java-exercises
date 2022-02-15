4781dd595d79a6e5241dc290ed60463553f8b7b1
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-14 19:22:33 -0600

    feat: equality printer exercise

--- src/exercises/EqualityPrinter.java
+++ src/exercises/EqualityPrinter.java
@@ -2,4 +2,18 @@ package exercises;
 
 public class EqualityPrinter {
 
+
+    public static void printEqual(int x, int y, int z) {
+        if(x < 0 || y < 0 || z < 0) {
+            System.out.println("Invalid Value");
+        }
+
+        if((x == y) && (y == z) && (z == x)) {
+            System.out.println("All numbers are equal");
+        } else if((x != z) && (x != y)) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither are different or equal");
+        }
+    }
 }
--- src/exercises/EqualityPrinterTest.java
+++ src/exercises/EqualityPrinterTest.java
@@ -4,5 +4,11 @@ public class EqualityPrinterTest {
 
     public static void main(String[] args) {
 
+
+         EqualityPrinter.printEqual(1, 1, 1);
+      EqualityPrinter.printEqual(1, 1, 2);
+         EqualityPrinter.printEqual(-1, -1, -1);
+       EqualityPrinter.printEqual(1, 2, 3);
+
     }
 }
