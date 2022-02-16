b283038794f4cb6c7b33e966f9360c00afe9d1fa
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-16 10:40:09 -0600

    feat: greatest common divisor

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,30 @@
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if((first < 10) || (second < 10)) {
+            return -1;
+        }
+
+        int tempValue;
+        int gcd = 0;
+
+        while(second != 0) {
+           tempValue = second;
+           second = first % second;
+           first = tempValue;
+            gcd = first;
+        }
+
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+
+
+    }
+}
