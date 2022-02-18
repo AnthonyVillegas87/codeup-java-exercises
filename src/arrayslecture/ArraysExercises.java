ef8a362141e408f6e4844af0e810f3c91a0c067b
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-18 12:40:35 -0600

    feat: algorithms: sort start

--- src/arrayslecture/ArraysExercises.java
+++ src/arrayslecture/ArraysExercises.java
@@ -18,6 +18,25 @@ public class ArraysExercises {
 
     public static void main(String[] args) {
 
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = 15;
+        intArray[3] = 21;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        int index = -1;
+        for(int i = 0; i < intArray.length; i++) {
+           if(intArray[i] == 55) {
+               index = i;
+               break;
+           }
+        }
+        System.out.println(index);
+
 //        List<Integer> numbers = new ArrayList<>();
 //        numbers.add(13);
 //        numbers.add(new Integer(8)); // Line 7
