141c4c9b07336d6e7f4218751435ee2ba7f7f409
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-22 13:02:05 -0600

    feat: merge interval intro

---
+++ src/patterns/merge_intervals/Interval.java
@@ -0,0 +1,11 @@
+package patterns.merge_intervals;
+
+class Interval {
+    int start;
+    int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
