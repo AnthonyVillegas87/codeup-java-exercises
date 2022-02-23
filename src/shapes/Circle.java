40af8a2adaec367629440bb2e9535ce34e20d5f4
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-22 21:48:47 -0600

    feat: shapes exercise revisited

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -4,16 +4,22 @@ public class Circle {
 
     protected double radius;
 
-//    public double getRadius() {
-//        return radius;
-//    }
+
 //
 //    public void setRadius(double radius) {
 //        this.radius = radius;
 //    }
 
-    Circle(double radius) {
+    public Circle(double radius) {
+        if(radius < 0) {
+            radius = 0;
+        }
         this.radius = radius;
+    }
+
+
+    public double getRadius() {
+        return radius;
     }
 
     public double getArea() {
