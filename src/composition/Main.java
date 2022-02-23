34d2d1b1965b0a59d70f2076d06cd55b9a552dc8
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-22 23:16:40 -0600

    feat: composition pt.2

--- src/composition/Main.java
+++ src/composition/Main.java
@@ -3,18 +3,34 @@ package composition;
 public class Main {
 
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-        Case theCase = new Case("220B", "Dell", "240", dimensions);
+//        Dimensions dimensions = new Dimensions(20, 20, 5);
+//        Case theCase = new Case("220B", "Dell", "240", dimensions);
+//
+//        Monitor monitor = new Monitor("27inch Premium", "Acer", 27, new Resolution(2540, 1440));
+//
+//        Motherboard motherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+//
+//        PC thePC = new PC(theCase, monitor, motherboard);
+//
+//       thePC.powerUp();
 
-        Monitor monitor = new Monitor("27inch Premium", "Acer", 27, new Resolution(2540, 1440));
 
-        Motherboard motherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
 
-        PC thePC = new PC(theCase, monitor, motherboard);
+        Ceiling ceiling = new Ceiling(12, 55);
 
-        thePC.getMonitor().drawPixelArt(1500,1200,"red");
-        thePC.getMotherboard().loadProgram("Windows 1.0");
-        thePC.getTheCase().pressPowerButton();
+        Bed bed = new Bed("King", 4, 3, 1, 1);
+
+        Lamp lamp = new Lamp("Modern", false, 75);
+
+        Room room = new Room("Anthony", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        room.makeBed();
+        room.getLamp().turnOn();
+
+
 
     }
 }
