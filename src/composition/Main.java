5690b9e06d76646eab0b43f8970ffdb83b88a406
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-01 22:16:24 -0600

    feat: encapsulation exercise

--- src/composition/Main.java
+++ src/composition/Main.java
@@ -15,22 +15,27 @@ public class Main {
 //       thePC.powerUp();
 
 
-        Wall wall1 = new Wall("West");
-        Wall wall2 = new Wall("East");
-        Wall wall3 = new Wall("South");
-        Wall wall4 = new Wall("North");
+//        Wall wall1 = new Wall("West");
+//        Wall wall2 = new Wall("East");
+//        Wall wall3 = new Wall("South");
+//        Wall wall4 = new Wall("North");
+//
+//        Ceiling ceiling = new Ceiling(12, 55);
+//
+//        Bed bed = new Bed("King", 4, 3, 1, 1);
+//
+//        Lamp lamp = new Lamp("Modern", false, 75);
+//
+//        Room room = new Room("Anthony", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+//        room.makeBed();
+//        room.getLamp().turnOn();
 
-        Ceiling ceiling = new Ceiling(12, 55);
-
-        Bed bed = new Bed("King", 4, 3, 1, 1);
-
-        Lamp lamp = new Lamp("Modern", false, 75);
-
-        Room room = new Room("Anthony", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
-        room.makeBed();
-        room.getLamp().turnOn();
-
-
+        Printer printer = new Printer(50, true);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new print count for printer =" + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new print count for printer =" + printer.getPagesPrinted());
 
     }
 }
