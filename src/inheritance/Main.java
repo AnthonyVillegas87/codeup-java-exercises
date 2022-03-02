0528b72c3b8b519f63b8f55180673851927ff9ab
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-02 12:58:32 -0600

    polymorphism revisit

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -2,6 +2,13 @@ package inheritance;
 
 public class Main {
 
+//    Inheritance
+//
+//    We can define classes that extend, or inherit from another class with the extends keyword in the class definition.
+//    We call these subclasses, and the classes that are being inherited from / extended superclasses.
+//
+//    A subclass inherits all the fields of it's superclass, and can re-use any of it's functionality.
+
 
     public static void main(String[] args) {
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
