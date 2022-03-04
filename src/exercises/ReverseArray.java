4a344fbcaa34c7df2501389614acb5b56ab1708e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-03 19:17:19 -0600

    feat: reverse array exercise

---
+++ src/exercises/ReverseArray.java
@@ -0,0 +1,28 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    private static void reverse(int[] array) {
+
+       int maxIndex = array.length - 1;
+       int halfLength = array.length / 2;
+
+        for(int i = 0; i < halfLength; i++) {
+           int temp = array[i];
+           array[i] = array[maxIndex - i];
+           array[maxIndex - i] = temp;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5, 6};
+        System.out.println("Array = " + Arrays.toString(myArray));
+
+        reverse(myArray);
+
+        System.out.println("My Array = " + Arrays.toString(myArray));
+    }
+}
