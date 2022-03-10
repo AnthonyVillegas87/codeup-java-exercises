6f4c468a82ef5ee7d5e19c3bcba3b2df88eb4245
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-10 02:23:28 -0600

    feat: bank exercise complete

--- src/bank_branch/Main.java
+++ src/bank_branch/Main.java
@@ -1,4 +1,29 @@
 package bank_branch;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        Bank bank = new Bank("Chase Bank");
+        bank.addBranch("Spring Branch");
+
+        bank.addCustomer("Spring Branch", "Boomero", 50.50);
+        bank.addCustomer("Spring Branch", "Tim", 255.00);
+        bank.addCustomer("Spring Branch", "Candy", 425.75);
+
+        bank.addBranch("San Antonio");
+        bank.addCustomer("San Antonio", "Nadine", 150.50);
+
+        bank.addCustomerTransaction("Spring Branch", "Tim", 45.22);
+        bank.addCustomerTransaction("Spring Branch", "Tim", 125.22);
+        bank.addCustomerTransaction("Spring Branch", "Boomero", 500.65);
+
+        bank.listCustomers("Spring Branch", true);
+        bank.listCustomers("San Antonio", true);
+
+        if(!bank.addCustomer("Cibolo", "Brian", 5.55)) {
+            System.out.println("Error Cibolo branch does not exist");
+        }
+
+    }
 }
