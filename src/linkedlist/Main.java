f37c97179b0025c1fda3bc5db7600f27e3efda95
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-15 16:19:27 -0500

    feat: linked list intro

---
+++ src/linkedlist/Main.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("Joe", 22.55);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+    }
+}
