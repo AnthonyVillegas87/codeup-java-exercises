6b7cd226471ab53ee898291bfb4e2c1d7fa440cc
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-15 17:34:53 -0500

    feat: pt 3 of linked list intro

--- src/linkedlist/Demo.java
+++ src/linkedlist/Demo.java
@@ -3,10 +3,11 @@ package linkedlist;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
 
-
+    //PRINT OUR LIST
     private static void printList(LinkedList<String> linkedList) {
         Iterator<String> i = linkedList.iterator();
 
@@ -16,7 +17,8 @@ public class Demo {
         System.out.println("===========================");
     }
 
-    private static boolean adInOrder(String newCity, LinkedList<String> linkedList) {
+    //ADD CITIES TO OUR LIST IN ALPHABETICAL ORDER
+    private static boolean addInOrder(String newCity, LinkedList<String> linkedList) {
         ListIterator<String> stringListIterator = linkedList.listIterator();
 
         while(stringListIterator.hasNext()) {
@@ -34,26 +36,110 @@ public class Demo {
         }
 
         stringListIterator.add(newCity);
+        return true;
     }
+
+    private static void visit(LinkedList<String> cities) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if(cities.isEmpty()) {
+            System.out.println("No cities in the itinerary");
+            return;
+        } else {
+            System.out.println("Now visiting " + listIterator.next());
+            printMenu();
+        }
+
+        while(!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Holiday (vacation) over");
+                    quit = true;
+                    break;
+
+                case 1:
+                    if(!goingForward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+
+                    } else {
+                        System.out.println("Reached the last city in the list");
+                        goingForward = false;
+                    }
+                    break;
+
+                case 2:
+                    if(goingForward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                        goingForward = true;
+                    }
+                    break;
+
+                case 3:
+                    printMenu();
+                    break;
+            }
+
+
+
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\npress ");
+        System.out.println("0 - to quit\n" +
+                "1 - go to next city\n" +
+                "2 - go to next city\n" +
+                "3 - print menu options");
+    }
+
 
 
     public static void main(String[] args) {
       LinkedList<String> placesToVisit = new LinkedList<>();
 
-      placesToVisit.add("Sydney");
-      placesToVisit.add("Melbourne");
-      placesToVisit.add("Brisbane");
-      placesToVisit.add("Perth");
-      placesToVisit.add("Canberra");
-      placesToVisit.add("Adelaide");
-      placesToVisit.add("Darwin");
-
+      addInOrder("Sydney", placesToVisit);
+      addInOrder("Melbourne",placesToVisit);
+      addInOrder("Brisbane",placesToVisit);
+      addInOrder("Perth",placesToVisit);
+      addInOrder("Canberra",placesToVisit);
+      addInOrder("Adelaide",placesToVisit);
+      addInOrder("Darwin", placesToVisit);
       printList(placesToVisit);
+      visit(placesToVisit);
 
-      placesToVisit.add(1, "Alice Springs");
-      printList(placesToVisit);
-
-      placesToVisit.remove(4);
-      printList(placesToVisit);
+//      placesToVisit.add("Sydney");
+//      placesToVisit.add("Melbourne");
+//      placesToVisit.add("Brisbane");
+//      placesToVisit.add("Perth");
+//      placesToVisit.add("Canberra");
+//      placesToVisit.add("Adelaide");
+//      placesToVisit.add("Darwin");
+//
+//      printList(placesToVisit);
+//
+//      placesToVisit.add(1, "Alice Springs");
+//      printList(placesToVisit);
+//
+//      placesToVisit.remove(4);
+//      printList(placesToVisit);
     }
 }
