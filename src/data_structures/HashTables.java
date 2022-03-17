88520c3bf19a4e59d05ad881701844759733703e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-17 15:12:02 -0500

    feat: intro to Hashing/Load Factor/ backing a Hash Table with an array

--- src/data_structures/HashTables.java
+++ src/data_structures/HashTables.java
@@ -11,4 +11,26 @@ public class HashTables {
     * Associative array is one type of hash table
      */
 
+    /*
+    // Hashing
+    * Maps keys of any data type to an integer
+    * Hash function maps keys to int
+    * In java, hash function is Object.hashCode()
+    * Collision occurs when more than one value has the same hashed value
+
+    // Load Factor
+    * Tells us how full a hash table is
+    * Load factor = # of items / capacity = size / capacity
+    * Load factor is used to decide when to resize the array backing the hash table
+    * Don't want load factor too low (a lot of empty space)
+    * Don't want load factor too high (will increase the likelihood of collisions)
+    * Can't play a role in determining the time complexity for retrieval
+
+    //====== Add to Hash Table backed by an array
+    * Provide a key/value pair
+    * Use a hash function to hash the key to an int value
+    * Store the value at the hashed key value - this is the index into the array
+     */
+
+
 }
