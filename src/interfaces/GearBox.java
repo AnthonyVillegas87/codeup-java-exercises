118cab522c1dec28f01e6372cc8a9c909e28ac8c
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-22 20:52:47 -0500

    feat: Interfaces revisit

---
+++ src/interfaces/GearBox.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class GearBox {
+
+    private boolean clutchIsIn;
+
+    public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
+    }
+
+}
---
+++ src/interfaces/Main.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // DECLARE THE INTERFACE CLASS TO USE SPECIFIC METHODS
+        ITelephone myPhone;
+        // INSTANTIATE THE CLASS THAT IMPLEMENT THESE METHODS
+        myPhone = new DeskPhone(2102218282);
+        myPhone.powerOn();
+        myPhone.callPhone(2102218282);
+        myPhone.answer();
+    }
+
+}
