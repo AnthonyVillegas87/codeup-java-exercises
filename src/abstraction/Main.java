192f5f4be7c8400ed3b7bedb581da7ae815f1a3d
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-25 20:04:08 -0500

    feat: abstraction revisit

---
+++ src/abstraction/Main.java
@@ -0,0 +1,21 @@
+package abstraction;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Yorkie");
+        dog.breath();
+        dog.eat();
+
+        Parrot bird = new Parrot("Australian ringneck");
+        bird.breath();
+        bird.eat();
+        bird.fly();
+
+        Penguin penguin = new Penguin("Emperor");
+        penguin.fly();
+    }
+
+
+}
