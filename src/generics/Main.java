62c75d781be0aff1fcf95d8668d376c7ed23cff6
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-31 23:56:42 -0500

    generics intro

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,4 +1,18 @@
 package generics;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    private static void printDouble(ArrayList<Integer> n) {
+        for(Object i : n) {
+            System.out.println((Integer) i * 2);
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(5);
+        items.add(10);
+        items.add(15);
+    }
 }
