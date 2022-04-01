3019db25593f7eb5423968912acf99472dae362e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-01 00:20:26 -0500

    feat: generics class pt 1

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -4,15 +4,27 @@ import java.util.ArrayList;
 
 public class Main {
 
-    private static void printDouble(ArrayList<Integer> n) {
-        for(Object i : n) {
-            System.out.println((Integer) i * 2);
-        }
-    }
+//    private static void printDouble(ArrayList<Integer> n) {
+//        for(int i : n) {
+//            System.out.println(i * 2);
+//        }
+//    }
     public static void main(String[] args) {
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(5);
-        items.add(10);
-        items.add(15);
+        FootballPlayer tom = new FootballPlayer("Tom");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer cristiano = new SoccerPlayer("Cristiano");
+
+        Team myTeam = new Team("Luchadores");
+        myTeam.addPlayer(tom);
+        myTeam.addPlayer(pat);
+        myTeam.addPlayer(cristiano);
+
+        System.out.println(myTeam.numPlayers());
+//        ArrayList<Integer> items = new ArrayList<>();
+//        items.add(5);
+//        items.add(10);
+//        items.add(15);
+//
+//        printDouble(items);
     }
 }
