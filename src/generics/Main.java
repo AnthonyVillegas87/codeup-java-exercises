3c0a13333443009481c46f3a683e8e8fe56ed392
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-05 23:30:00 -0500

    feat: generics exercise

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -14,37 +14,56 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer cristiano = new SoccerPlayer("Cristiano");
 
-        Team<FootballPlayer> tampa = new Team<>("Tampa Bay");
-        tampa.addPlayer(tom);
+//        Team<FootballPlayer> tampa = new Team<>("Tampa Bay");
+//        tampa.addPlayer(tom);
+//
+//
+//        System.out.println(tampa.numPlayers());
+//        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Chicago Cubs");
+//        baseballPlayerTeam.addPlayer(pat);
+//
+//        Team<SoccerPlayer> soccerPlayerTeam = new Team<>("Portugal");
+//        soccerPlayerTeam.addPlayer(cristiano);
+//
+//
+//
+//
+//        Team<FootballPlayer> miami = new Team<>("Miami Dolphins");
+//        FootballPlayer tua = new FootballPlayer("Tua Tagovailoa");
+//        miami.addPlayer(tua);
+//
+//        Team<FootballPlayer> newEngland = new Team<>("New England Patriots");
+//        Team<FootballPlayer> indianapolis = new Team<>("Indianapolis Colts");
+//        newEngland.matchResult(indianapolis,7, 10 );
+//        newEngland.matchResult(miami, 21, 6);
+//
+//        tampa.matchResult(miami, 24, 21);
+//
+//        System.out.println("Rankings");
+//        System.out.println(miami.getName() + ": " + miami.ranking());
+//        System.out.println(newEngland.getName() + ": " + newEngland.ranking());
+//        System.out.println(tampa.getName() + ": " + tampa.ranking());
+//        System.out.println(indianapolis.getName() + ": " + indianapolis.ranking());
+//        System.out.println(miami.compareTo(tampa));
+//        System.out.println(newEngland.compareTo(indianapolis));
 
 
-        System.out.println(tampa.numPlayers());
-        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Chicago Cubs");
-        baseballPlayerTeam.addPlayer(pat);
-
-        Team<SoccerPlayer> soccerPlayerTeam = new Team<>("Portugal");
-        soccerPlayerTeam.addPlayer(cristiano);
-
-
-
-
+        League<Team<FootballPlayer>> footballLeague = new League<>("NFL");
         Team<FootballPlayer> miami = new Team<>("Miami Dolphins");
-        FootballPlayer tua = new FootballPlayer("Tua Tagovailoa");
-        miami.addPlayer(tua);
-
         Team<FootballPlayer> newEngland = new Team<>("New England Patriots");
         Team<FootballPlayer> indianapolis = new Team<>("Indianapolis Colts");
+        Team<FootballPlayer> tampa = new Team<>("Tampa Bay");
+
         newEngland.matchResult(indianapolis,7, 10 );
         newEngland.matchResult(miami, 21, 6);
-
         tampa.matchResult(miami, 24, 21);
 
-        System.out.println("Rankings");
-        System.out.println(miami.getName() + ": " + miami.ranking());
-        System.out.println(newEngland.getName() + ": " + newEngland.ranking());
-        System.out.println(tampa.getName() + ": " + tampa.ranking());
-        System.out.println(indianapolis.getName() + ": " + indianapolis.ranking());
-        System.out.println(miami.compareTo(tampa));
-        System.out.println(newEngland.compareTo(indianapolis));
+        footballLeague.add(miami);
+        footballLeague.add(newEngland);
+        footballLeague.add(indianapolis);
+        footballLeague.add(tampa);
+
+        footballLeague.showLeagueTable();
+
     }
 }
