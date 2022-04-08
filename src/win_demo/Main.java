1f1a5b8ea989e1d4d799bb1fe767b14e8fd296f5
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-07 23:15:44 -0500

    feat: Java window/frame package intro

---
+++ src/win_demo/Main.java
@@ -0,0 +1,10 @@
+package win_demo;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Window package");
+        myWindow.setVisible(true);
+    }
+}
---
+++ src/win_demo/MyWindow.java
@@ -0,0 +1,33 @@
+package win_demo;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyWindow extends Frame {
+
+
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+
+        g.setFont(sansSerifLarge);
+        g.drawString("My first Java String Package", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("Anthony Villegas", 60,100);
+
+    }
+}
