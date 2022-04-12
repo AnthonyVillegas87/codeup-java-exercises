1058416ecd4617c56ffb908cf20e8be75de7b9e6
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-11 21:28:43 -0500

    feat: static & final keywords

--- src/static_keyword/Main.java
+++ src/static_keyword/Main.java
@@ -1,12 +1,17 @@
 package static_keyword;
 
 public class Main {
-
+    public static int multiplier = 7;
     public static void main(String[] args) {
-        StaticTest firstInstance = new StaticTest("1st Instance");
-        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
-        StaticTest secondInstance = new StaticTest("2nd Instance");
-        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//        StaticTest firstInstance = new StaticTest("1st Instance");
+//        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest secondInstance = new StaticTest("2nd Instance");
+//        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+    }
+
+    public static int multiply(int number) {
+        return number * multiplier;
     }
 }
