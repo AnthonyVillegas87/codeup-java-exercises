7f242a557e7d8f526b83243abe169a25941fd3b6
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-12 00:02:11 -0500

    feat: collections overview

---
+++ src/collections/Main.java
@@ -0,0 +1,15 @@
+package collections;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        //theatre.getSeats();
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Ready for payment");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
