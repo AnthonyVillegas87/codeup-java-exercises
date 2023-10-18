4b4d6937c08b08946b819d77c52e5078fa47ce79
Anthony Villegas <villegasanthony87@yahoo.com>
2023-10-17 22:14:32 -0500

    my roadmap for this month! In code

---
+++ java8project/src/java_basics/Hello.java
@@ -0,0 +1,26 @@
+package java_basics;
+
+import java.sql.SQLOutput;
+
+public class Hello {
+
+
+    public static void main(String[] args) {
+        System.out.println("Going forward, we will be refreshing our Java proficiency.");
+        System.out.print("Let's have fun with this.");
+        System.out.println("Pt 1: Very Basics.... Yes, that's right AGAIN! ");
+        System.out.println("Pt 2: Understanding Methods.");
+        System.out.println("Pt 3: OOP Dive! (Object Oriented Programming) ");
+        System.out.println("Pt 4: Lets put together with String, Nested Loops and a Project!!!");
+        System.out.println("Pt 5: Building & deploying JAR files.");
+        System.out.println("Pt 6: File Processing & Error Handling.");
+        System.out.println("Pt 7: Collections Frameworks & DSA!");
+        System.out.println("Another Project....");
+        System.out.println("Pt 8: Concurrency in Java");
+        System.out.println("Pt 9: JDBC Connectivity");
+        System.out.println("Pt 10: Lambda Expressions & Streams API");
+
+
+    }
+
+}
