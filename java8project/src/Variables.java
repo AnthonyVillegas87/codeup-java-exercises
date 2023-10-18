9304e92376f4b9856c374cb4d2a93b397035d56f
Anthony Villegas <villegasanthony87@yahoo.com>
2023-10-17 23:06:59 -0500

    Variables from memory

---
+++ java8project/src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+
+    public static void main(String[] args) {
+     int x = 4;
+        System.out.println(x);
+        x = 34;
+        System.out.println(x);
+
+        System.out.println(x + 28);
+
+        String myWords;
+        myWords = "No need for a type mismatch here, you know what to do!";
+        System.out.println(myWords + " " +  -x);
+    }
+
+
+}
---
+++ java8project/src/VariablesContinued.java
@@ -0,0 +1,17 @@
+public class VariablesContinued {
+
+
+    public static void main(String[] args) {
+        int var = 10000000;
+        long bigNum = 10000000000000L;
+        short shortNum = 32767; // Max for which this can hold
+            // Min -32768
+        byte verySmallNum = 127; // Max for which this can hold
+            // Min -128
+
+        double decimalNum = 394.002;
+        boolean decide = false;
+        char letter = 'C';
+    }
+
+}
