01f4709075ff48e5ad1492f514254e1d4557801a
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-14 10:23:42 -0600

    Constructor implemented & test complete

--- emailapplication/src/Email.java
+++ emailapplication/src/Email.java
@@ -9,8 +9,12 @@ public class Email {
     private String alternateEmail;
 
 
-    //Constructor to receive first & last name
-
+    // Constructor to receive first & last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+    }
     // Ask for department
 
     // Generate a random password
