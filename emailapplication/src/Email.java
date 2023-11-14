d303772986eefe079d4d84b2e6caee63df404bed
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-14 12:07:28 -0600

    Method to generate a random password created

--- emailapplication/src/Email.java
+++ emailapplication/src/Email.java
@@ -7,6 +7,7 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private int defaultPasswordLength = 10;
     private int mailboxCapacity;
     private String alternateEmail;
 
@@ -20,6 +21,10 @@ public class Email {
         // Call a method asking for the department - return the department
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        // Call a method to return a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
 
 
@@ -40,8 +45,20 @@ public class Email {
             return "not available";
         }
     }
-    // Generate a random password
 
+
+    // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+        char[] password = new char[length];
+
+        for(int i = 0; i < length; i++) {
+          int random = (int)(Math.random() * passwordSet.length());
+          password[i] = passwordSet.charAt(random);
+        }
+
+        return new String(password);
+    }
     // Set the mailbox capacity
 
     // Set an alternate email
