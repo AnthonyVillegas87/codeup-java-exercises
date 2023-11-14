532638a9a92228002ce64ba4bde7934a0d6bde09
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-14 10:50:01 -0600

    Defined the Email Department

--- emailapplication/src/Email.java
+++ emailapplication/src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
 
     // Encapsulate using private access
@@ -14,9 +16,30 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
-    }
-    // Ask for department
 
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+
+
+    // Ask for department: NOTE, THIS IS NOT TO BE PUBLIC
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\nEnter the department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if (depChoice == 1) {
+            return "sales";
+        } else if(depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else {
+            return "not available";
+        }
+    }
     // Generate a random password
 
     // Set the mailbox capacity
