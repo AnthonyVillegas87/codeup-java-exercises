cfeb29203c882ded60214dd567374d54478f17e9
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-15 19:15:14 -0600

    Implementing the enrollment method

--- student_database_app/src/Student.java
+++ student_database_app/src/Student.java
@@ -31,8 +31,8 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentId;
-    private String courses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
 
@@ -63,7 +63,26 @@ public class Student {
         id++;
        this.studentId = gradeYear + "" + id;
     }
+
+
     // Enroll in courses
+    public void enroll() {
+        // Iterate with a loop until user is finished enrolling
+        do {
+            System.out.print("Enter course to enroll in (0 to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if(!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                break;
+            }
+        } while (1 != 0);
+
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition Balance: " + tuitionBalance);
+    }
 
     // View balance
 
--- student_database_app/src/StudentDatabaseApplication.java
+++ student_database_app/src/StudentDatabaseApplication.java
@@ -4,6 +4,7 @@ public class StudentDatabaseApplication {
 
     public static void main(String[] args) {
         Student student1 = new Student();
+        student1.enroll();
         // Ask how many new users we want to add
 
 
