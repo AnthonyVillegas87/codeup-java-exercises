99c984cec36c41fd52a258d26e44fd4fd252e36a
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-15 18:30:13 -0600

    Student class beginning set-up.

---
+++ student_database_app/src/Student.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Student {
+    /*
+     *           Student Database Application -
+     *      as a Database Admin for a university, you need to create an application to manage student enrollments and balance
+     *
+     *  Ask the user how many students will be added to the database
+     *  The user should be prompted to enter the name and year for each student
+     *  The student should have a 5-digit unique-ID, with the first number being their grade level
+     *  A student can enroll in the following courses:
+     *   History 101
+     *   Mathematics 101
+     *   English 101
+     *   Chemistry 101
+     *   Computer Science 101
+     *
+     *  Each course costs $600 to enroll
+     *  The student should be able to view their balance and pay the tuition
+     *  To see the status of the student, we should see their name, ID, courses enrolled, and balance.
+     *
+     *
+     *
+     *
+     * */
+
+
+
+
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+
+    // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter student's first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student's last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level:  ");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
+
+    // Generate ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
+}
--- student_database_app/src/StudentDatabaseApplication.java
+++ student_database_app/src/StudentDatabaseApplication.java
@@ -1,29 +1,14 @@
 public class StudentDatabaseApplication {
 
-    /*
-    *           Student Database Application -
-    *      as a Database Admin for a university, you need to create an application to manage student enrollments and balance
-    *
-    *  Ask the user how many students will be added to the database
-    *  The user should be prompted to enter the name and year for each student
-    *  The student should have a 5-digit unique-ID, with the first number being their grade level
-    *  A student can enroll in the following courses:
-    *   History 101
-    *   Mathematics 101
-    *   English 101
-    *   Chemistry 101
-    *   Computer Science 101
-    *
-    *  Each course costs $600 to enroll
-    *  The student should be able to view their balance and pay the tuition
-    *  To see the status of the student, we should see their name, ID, courses enrolled, and balance.
-    *
-    *
-    *
-    *
-    * */
 
 
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        // Ask how many new users we want to add
+
+
+       // Create N number of new students
+    }
 
 
 
