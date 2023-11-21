90212ca3204184535f73a81006f5e98308b833e7
Anthony Villegas <villegasanthony87@yahoo.com>
2023-11-21 16:13:37 -0600

    Bank Application start

---
+++ bank_app/src/Bank.java
@@ -0,0 +1,53 @@
+public class Bank {
+
+    /*
+    *       Bank Application - as a back-end developer you need to create an application to handle new customer bank account requests
+    *
+    *
+    *   Read a .csv file of names, social security numbers, account type, and initial deposit.
+    *   Use a proper data structure to hold all of these accounts
+    *   Both savings and checking accounts share the following properties:
+    *
+    *   deposit()
+    *   withdraw()
+    *   transfer()
+    *   showInfo()
+    *
+    *
+    *   11-Digit Account Number( generated w/ the following process :
+    *           1 or 2 depending on Savings or Checking,
+    *           last two digits of SSN,
+    *           unique 5-digit number,
+    *           and random 3-digit number
+    *           )
+    *
+    *   Savings Account holders are given a Safe deposit box, identified by a 3-digit number and
+    *   accessed with a 4-digit code
+    *
+    *   Checking account holders are assigned a Debit Card w/ a 12-digit number & 4-digit PIN
+    *
+    *   Both accounts will use an interface the determines the best interest rate.
+    *
+    *       Savings accounts will use .25 points less than the base rate
+    *       Checking accounts will use 15% of the base rate
+    *
+    *   The ShowInfo method should reveal relevant account information as well as information specific
+    *   to the Checking account or Savings account;
+    *
+    *
+    *  Objectives:
+    * Develop a robust application architecture
+    * Use when to use abstract classes & abstract methods
+    * Use an interface API to receive information from a developer's application
+    * Explore constructors deeper & use the super() keyword
+    * Read data from a file & store in an appropriate data structure
+    * Generate random numbers & work with String API
+    *
+    *  */
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
